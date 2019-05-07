4ee8155c2330f82eeaa363fdadae2303f6ec989b
Simas2006 <sjuknelis@gmail.com>
2019-05-06 21:44:37 -0400

    Redirect from / to voting page

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var votesReleased = false;
 
 app.use("/public",express.static(__dirname + "/public"));
 
+app.get("/",function(req,res) {
+  res.redirect("/public/voting");
+});
+
 function setAll(state) {
   for ( var i = 0; i < activeUIDs.length; i++ ) {
     lockedUIDs[activeUIDs[i]] = state;
